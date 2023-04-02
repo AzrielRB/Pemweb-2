4539f6b3a8a1366ffca1eb42f39860918ef44972
AzrielRB <azrielbilal4@gmail.com>
2023-04-02 20:18:33 +0700

    Pertemuan 4

---
+++ praktikum4/addSiswa.php
@@ -0,0 +1,42 @@
+<?php
+    include_once 'template\header.php';
+
+?>
+
+
+<form>
+  <div class="form-group row">
+    <label for="text" class="col-4 col-form-label">Text</label> 
+    <div class="col-8">
+      <input id="text" name="text" type="text" class="form-control">
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="text1" class="col-4 col-form-label">Text Field</label> 
+    <div class="col-8">
+      <input id="text1" name="text1" type="text" class="form-control">
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="text2" class="col-4 col-form-label">Text Field</label> 
+    <div class="col-8">
+      <input id="text2" name="text2" type="text" class="form-control">
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="text3" class="col-4 col-form-label">Text Field</label> 
+    <div class="col-8">
+      <input id="text3" name="text3" type="text" class="form-control">
+    </div>
+  </div> 
+  <div class="form-group row">
+    <div class="offset-4 col-8">
+      <button name="submit" type="submit" class="btn btn-primary">Submit</button>
+    </div>
+  </div>
+</form>
+
+<?php
+    include_once 'template\footer.php';
+
+?>
